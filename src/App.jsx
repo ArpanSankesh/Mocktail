d625bcb8aff22a1065a70671b2c0bd7b66577e92
ArpanSankesh <arpansankesh830@gmail.com>
2025-06-24 22:24:17 +0530

    Setup : tailwind and gsap setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all"
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
 const App = () => {
   return (
-    <div>App</div>
+    <div className="flex-center h-[100vh]">
+      <h1 className="text-3xl ">Hello, BRO</h1>
+    </div>
   )
 }
 
